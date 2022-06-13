d0ce6d4c602619c4a5cc22f8744689806d6ff25f
NatalaBoiko <92225403+NatalaBoiko@users.noreply.github.com>
2022-06-13 15:21:14 +0000

    Deploying to gh-pages from @ NatalaBoiko/test-project@bcb7a6da3d87548b97d18f02c6c28132eb25a56a 🚀

--- index.52bc0c13.js
+++
@@ -1,2 +0,0 @@
-fetch("https://api.themoviedb.org/3/search/movie?api_key={API_KEY}&query=Jack+Reacher").then((e=>console.log(e)));
-//# sourceMappingURL=index.52bc0c13.js.map
--- index.d4ccc242.js
+++
@@ -1,2 +0,0 @@
-fetch("https://api.themoviedb.org/3/search/movie?api_key={API_KEY}&query=Jack+Reacher").then((function(e){return console.log(e)}));
-//# sourceMappingURL=index.d4ccc242.js.map
